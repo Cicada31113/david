f26750c80484640a1961d44b7cd1560aacf9a754
덕현 <stiryouup2@gmail.com>
2025-08-05 20:29:45 +0900

    Feat: 메뉴 화면추가
    /menu 라우트 추가 및 menu.html 템플릿 작성 -m Resolves:

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
-echo "print('Hello, Codyssey!')" > app.py
+# app.py
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '홈 페이지'
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
