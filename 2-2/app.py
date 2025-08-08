cb53c1d6fcc072368a6909fb54bf21ca4076500c
덕현 <stiryouup2@gmail.com>
2025-08-08 14:40:58 +0900

    TEST2 AMMEND 명령어 사용중

--- 2-2/app.py
+++ 2-2/app.py
@@ -7,30 +7,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 print("✅ Flask 앱 생성 완료")
 
-@app.route('/test2')  
-def test2():
-    print("🚀 '/' 경로에 접속함") 
 
-    if app.debug:   # 개발 모드(debug=True)일 때만 실행
-        print("🛠 디버그 모드 ON")  
-        hostname = '컴퓨터(인스턴스) : ' + socket.gethostname()
-        print(f"📡 hostname 설정됨 → {hostname}")
-
-    else:
-        print("🔒 운영모드 (디버그 아님)")
-        hostname = ' '      # 디버그 모드가 아닐 때 아래처럼 실행
-    
-    print("📄 index.html 렌더링 시작")
-    return render_template('test2.html')
-
-
-                     # socket.gethostname()
-                     # 현재 컴퓨터의 이름을 가져옴
-                     # hostname 변수에 그 이름을 붙여 문자열로 저장
-
-                     # render_template('index.html', computername=hostname)
-                     # templates 폴더에 있는 index.html 파일을 불러오고
-                     # 거기에 computername 이라는 값으로 hostname을 넘겨줌
 
 if __name__ == '__main__':
     print("🚦 서버 실행 준비 완료")
